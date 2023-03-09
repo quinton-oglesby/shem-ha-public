a0a5c9ba0733458eb5152a8eac8ca399634e2f0a
Sapphire <quinton.oglesby@icloud.com>
2023-03-08 18:02:41 -0600

    add connections

--- main.go
+++ main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+// Creating a struct to hold the Discord and OpenAI Token held within tokens.json.
+type Tokens struct {
+	DiscordToken string
+	GPT3Token    string
+}
+
+// Making a struct to hold the MySQL server logon parameters.
+type MySQLParameters struct {
+	Username string
+	Password string
+	Database string
+}
+
+// Globalizing the Tokens struct to hold the data.
+var tokens Tokens
+
+// Globalizing the MySQLParameters struct to hold the data.
+var mySQLParameters MySQLParameters
+
+// Global variable to hold database connection.
+var db *sql.DB
+
+// Global variable to hold the regex string
+var re *regexp.Regexp
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	// Retrieve the tokens from the tokens.json file.
+	tokensFile, err := os.ReadFile("tokens.json")
+	if err != nil {
+		log.Fatal("COULD NOT READ 'tokens.json' FILE: ", err)
+	}
+
+	// Retrieve the parameters from db_data.json file.
+	dbParameters, err := os.ReadFile("db_data.json")
+	if err != nil {
+		log.Println("Could not open sql_data file.")
+		log.Fatal(err)
+	}
+
+	// Unmarshal the tokens and database parameters.
+	json.Unmarshal(tokensFile, &tokens)
+	json.Unmarshal(dbParameters, &mySQLParameters)
+
+	// Compile regex string.
+	re, _ = regexp.Compile(`([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?`)
+
+	// Set up the parameters for the database connection.
+	configuration := mysql.Config{
+		User:   mySQLParameters.Username,
+		Passwd: mySQLParameters.Password,
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: mySQLParameters.Database,
+	}
+
+	// Open a connection to the discord_messages database.
+	db, err = sql.Open("mysql", configuration.FormatDSN())
+	if err != nil {
+		log.Println(err)
+	}
 }
