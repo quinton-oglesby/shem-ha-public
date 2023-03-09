18a7ab1214798ae35bb0963b98e630ad01c32549
Sapphire <quinton.oglesby@icloud.com>
2023-03-09 05:51:37 -0600

    add setup checks

--- commandHandlerMap.go
+++ commandHandlerMap.go
@@ -289,6 +289,17 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 		}
 	},
 	"chat_get_chance": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+		if !guildIsSetup(interaction.GuildID) {
+			//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "You need to run /setup first!",
+				},
+			})
+			return
+		}
+
 		// Responding to the interaction.
 		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -299,6 +310,17 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 		})
 	},
 	"chat_set_chance": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+		if !guildIsSetup(interaction.GuildID) {
+			//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "You need to run /setup first!",
+				},
+			})
+			return
+		}
+
 		chance := interaction.ApplicationCommandData().Options[0].FloatValue()
 
 		// Creating a single row query to set the chance of the chat response chance.
@@ -325,6 +347,17 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 		})
 	},
 	"chat_get_length": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+		if !guildIsSetup(interaction.GuildID) {
+			//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "You need to run /setup first!",
+				},
+			})
+			return
+		}
+
 		// Responding to the interaction.
 		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -335,6 +368,17 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 		})
 	},
 	"chat_set_length": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+		if !guildIsSetup(interaction.GuildID) {
+			//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "You need to run /setup first!",
+				},
+			})
+			return
+		}
+
 		tokens := interaction.ApplicationCommandData().Options[0].IntValue()
 
 		// Creating a single row query to set the chance of the chat response chance.
@@ -360,247 +404,4 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 			},
 		})
 	},
-
-	//"set_chance": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	//	gpt3Parameters.Chance = interaction.ApplicationCommandData().Options[0].FloatValue()
-	//
-	//	// Marshall the new parameters to save.
-	//	jsonBytes, err := json.Marshal(gpt3Parameters)
-	//	if err != nil {
-	//		log.Println("ERROR MARSHALING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE CHANCE: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Save updated parameters into parameters.json.
-	//	err = os.WriteFile("parameters.json", jsonBytes, 0644)
-	//	if err != nil {
-	//		log.Panicln("ERROR SAVING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE CHANCE: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Responding to the interaction.
-	//	//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
-	//	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//		Data: &discordgo.InteractionResponseData{
-	//			Content: fmt.Sprintf("Successfully updated the response chance. The reponse chance is now %v percent.", gpt3Parameters.Chance),
-	//		},
-	//	})
-	//},
-	//"set_length": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	//	gpt3Parameters.Length = interaction.ApplicationCommandData().Options[0].IntValue()
-	//
-	//	// Marshall the new parameters to save.
-	//	jsonBytes, err := json.Marshal(gpt3Parameters)
-	//	if err != nil {
-	//		log.Println("ERROR MARSHALING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE LENGTH: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Save updated parameters into parameters.json.
-	//	err = os.WriteFile("parameters.json", jsonBytes, 0644)
-	//	if err != nil {
-	//		log.Println("ERROR SAVING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE LENGTH: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Responding to the interaction.
-	//	//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
-	//	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//		Data: &discordgo.InteractionResponseData{
-	//			Content: fmt.Sprintf("Successfully updated the response length. The reponse length is now %v tokens.", gpt3Parameters.Length),
-	//		},
-	//	})
-	//},
-	//"pop_channel": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	//	// Snagging the target channel ID.
-	//	targetChannelName := interaction.ApplicationCommandData().Options[0].ChannelValue(session).Name
-	//	targetChannelID := interaction.ApplicationCommandData().Options[0].ChannelValue(session).ID
-	//
-	//	// Checking if channel is already in the list of approved channels.
-	//	if !stringInArray(targetChannelID, channels.Channels) {
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("Channel '%v' is not in the list of approved channels.", targetChannelName),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Remove channel from the list of channels allowed.
-	//	channels.Channels = removeStringFromArray(targetChannelID, channels.Channels)
-	//
-	//	// Marshall the new channels to save.
-	//	jsonBytes, err := json.Marshal(channels)
-	//	if err != nil {
-	//		log.Println("ERROR MARSHALING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE CHANNELS: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Save updated parameters into parameters.json.
-	//	err = os.WriteFile("channels.json", jsonBytes, 0644)
-	//	if err != nil {
-	//		log.Println("ERROR SAVING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE CHANNELS: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Responding to the interaction.
-	//	//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
-	//	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//		Data: &discordgo.InteractionResponseData{
-	//			Content: fmt.Sprintf("Successfully removed '%v' from the list of channels I am allowed to respond in.", targetChannelName),
-	//		},
-	//	})
-	//},
-	//"append_channel": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	//
-	//	// Snagging the target channel ID.
-	//	targetChannelName := interaction.ApplicationCommandData().Options[0].ChannelValue(session).Name
-	//	targetChannelID := interaction.ApplicationCommandData().Options[0].ChannelValue(session).ID
-	//
-	//	// Checking if channel is already in the list of approved channels.
-	//	if stringInArray(targetChannelID, channels.Channels) {
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("Channel '%v' is already in the list of approved channels.", targetChannelName),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Add channel to the list of channels allowed.
-	//	channels.Channels = append(channels.Channels, targetChannelID)
-	//
-	//	// Marshall the new channels to save.
-	//	jsonBytes, err := json.Marshal(channels)
-	//	if err != nil {
-	//		log.Println("ERROR MARSHALING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE CHANNELS: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Save updated parameters into parameters.json.
-	//	err = os.WriteFile("channels.json", jsonBytes, 0644)
-	//	if err != nil {
-	//		log.Println("ERROR SAVING JSON: ", err)
-	//
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: fmt.Sprintf("FAILED TO UPDATE CHANNELS: %v", err),
-	//			},
-	//		})
-	//
-	//		return
-	//	}
-	//
-	//	// Responding to the interaction.
-	//	//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
-	//	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//		Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//		Data: &discordgo.InteractionResponseData{
-	//			Content: fmt.Sprintf("Successfully added '%v' to the list of channels I am allowed to respond in.", targetChannelName),
-	//		},
-	//	})
-	//},
-	//"list_channels": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	//
-	//	if len(channels.Channels) > 0 {
-	//		chnls := ""
-	//		for _, channelID := range channels.Channels {
-	//			channel, err := session.Channel(channelID)
-	//			if err != nil {
-	//				log.Println("ERROR RETREIVING CHANNELS: ", err)
-	//
-	//				session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//					Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//					Data: &discordgo.InteractionResponseData{
-	//						Content: fmt.Sprintf("FAILED TO GET CHANNELS: %v", err),
-	//					},
-	//				})
-	//				return
-	//			}
-	//
-	//			chnls += channel.Name + "\n"
-	//		}
-	//
-	//		// Responding to the interaction.
-	//		//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: chnls,
-	//			},
-	//		})
-	//	} else {
-	//		session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-	//			Type: discordgo.InteractionResponseChannelMessageWithSource,
-	//			Data: &discordgo.InteractionResponseData{
-	//				Content: "CHANNEL LIST IS EMPTY",
-	//			},
-	//		})
-	//	}
-	//
-	//},
 }
